10bfcae96b705e7445b560a77539853337d65b9b
zxw <zxw253163415@163.com>
2022-01-02 15:30:23 +0800

    springboot整合Druid

---
+++ src/test/java/project/DruidTest.java
@@ -0,0 +1,29 @@
+package project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DruidTest {
+    @Autowired
+    DataSource dataSource;
+    /*
+     * HikariDataSource 默认数据源，性能很高
+     * DruidDataSource 使用很高，很稳定
+     */
+    @Test
+    public void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+}
