d930755aa5f77d71e494e714e76e0b995869fd7f
zxw <zxw253163415@163.com>
2022-01-02 15:06:25 +0800

    springboot整合Junit

---
+++ src/test/java/project/AppTest.java
@@ -0,0 +1,27 @@
+package project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import project.bean.User;
+import project.service.UserService;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+    @Autowired
+    UserService userService;
+    @Test
+    public void test(){
+        List<User> all = userService.findAll();
+        for (User user : all) {
+            System.out.println(user);
+        }
+    }
+
+}
